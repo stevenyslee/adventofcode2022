3a745fee0c609ff3f04b0b1fd4f8fe2c97b5334b
Steven Lee <stevenyslee@gmail.com>
2022-12-22 00:33:26 -0800

    Day 1 part 2 solution

--- 1/1.js
+++ 1/1.js
@@ -1,4 +1,8 @@
-const fs = require('fs');
+import * as fs from 'fs';
+import {
+    MaxHeap
+} from '@datastructures-js/heap';
+
 
 const example = `1000
 2000
@@ -22,7 +26,7 @@ function mostCalories(elvesCalorieInput) {
 
     for (let i = 0; i < calorieArray.length; i++) {
         let calorie = parseInt(calorieArray[i]);
-        if (isNaN(calorie)) {
+        if (isNaN(calorie) || i === calorieArray.length - 1) {
             maxCalorie = Math.max(maxCalorie, currentCalorie);
             currentCalorie = 0;
         } else {
@@ -35,10 +39,33 @@ function mostCalories(elvesCalorieInput) {
 
 console.assert(mostCalories(example) === 24000);
 
+function topThreeCalorieSum(elvesCalorieInput) {
+    let currentCalorie = 0;
+    let heap = new MaxHeap();
+    const calorieArray = elvesCalorieInput.split("\n");
+
+    for (let i = 0; i < calorieArray.length; i++) {
+        let calorie = parseInt(calorieArray[i]);
+        if (isNaN(calorie)) {
+            heap.insert(currentCalorie);
+            currentCalorie = 0;
+        } else if (i === calorieArray.length - 1) {
+            heap.insert(currentCalorie + calorie);
+        } else {
+            currentCalorie += calorie;
+        }
+    }
+    return heap.extractRoot() + heap.extractRoot() + heap.extractRoot();
+}
+
+console.assert(topThreeCalorieSum(example) === 45000);
+
 fs.readFile('/Users/stevenyslee/Projects/adventofcode2022/1/input.txt', 'utf8', (err, data) => {
     if (err) {
         console.error(err);
         return;
     }
     console.log("mostCalories: " + mostCalories(data));
-}); 
+    console.log("topThreeCalorieSum: " + topThreeCalorieSum(data));
+});
+
